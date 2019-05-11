32495394e56e90533af9d04482536e1b5644814c
Daniel K <FredyC@users.noreply.github.com>
2019-05-11 08:44:26 +0200

    Initial project setup

---
+++ doczrc.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'MobX with React',
+  description: 'Make your components truly reactive with MobX',
+  themeConfig: {
+    mode: 'dark',
+  },
+  files: 'docs/**/*.{md,mdx}',
+  typescript: true,
+  propsParser: false,
+}
