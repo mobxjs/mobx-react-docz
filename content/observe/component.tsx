1faab819c99ef59e580a6a4c522c5a8c11ce076a
Daniel K <fredy.c@seznam.cz>
2019-05-11 17:44:44 +0200

    Cover all observer variants

---
+++ content/observe/component.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Observer, useLocalStore } from 'mobx-react-lite'
+
+export default function ObservePerson() {
+  const person = useLocalStore(() => ({ name: 'John' }))
+  return (
+    <div>
+      {person.name} <i>I will never change my name</i>
+      <div>
+        <Observer>{() => person.name}</Observer>
+        {'   '}
+        <button onClick={() => (person.name = 'Mike')}>
+          I want to be Mike
+        </button>
+      </div>
+    </div>
+  )
+}
